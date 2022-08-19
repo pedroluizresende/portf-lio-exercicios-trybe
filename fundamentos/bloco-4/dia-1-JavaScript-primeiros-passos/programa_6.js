fd23661958f907dab63cf62f7c8cfa0875665264
Pedro Resende <pedroluizrnunez@gmail.com>
2022-08-19 02:02:53 -0300

    programas do 1 ao 11

--- fundamentos/bloco-4/dia-1-JavaScript-primeiros-passos/programa_6.js
+++ fundamentos/bloco-4/dia-1-JavaScript-primeiros-passos/programa_6.js
@@ -1,1 +1,23 @@
 // Programa 6
+let pecaXadrez= 'Rei';
+
+switch (pecaXadrez) {
+    case 'Peão':
+        console.log('Anda 1 para frente e como na diagonal')
+        break;
+    case 'Torre':
+        console.log('Movimenta-se na horizontal e vertical número de casas ilimitados')
+        break
+    case 'Cavalo':
+        console.log('Movimenta-se em L 3 para frente e 1 para o lado')
+        break
+    case 'Bispo':
+        console.log('Movimenta-se nas diagonais, número de casas ilimitado')
+        break
+    case 'Rainha':
+        console.log('Movimenta-se em todas as direções, número de casas ilimitado')
+        break
+    case 'Rei':
+        console.log('Movimenta-se em todas direções, apenas 1 casa por rodada')
+
+}
