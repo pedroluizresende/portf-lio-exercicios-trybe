b27d0b13d60df9abad04a6fe7f7203326f71a9f7
Pedro Resende <pedroluizrnunez@gmail.com>
2022-08-23 01:12:30 -0300

    Exercicios bonus do 1 ao 4

--- fundamentos/bloco-4/dia-3-logica-de-programacao-e-algoritmo/exercicio.js
+++ fundamentos/bloco-4/dia-3-logica-de-programacao-e-algoritmo/exercicio.js
@@ -32,15 +32,84 @@
 
 // let numerosPrimos = [];
 
-for (let index = 2; index <= 50; index +=1){
-    let divisor = 0;
-    for(let index2 = 2; index2 < index; index2 +=1){
-        if (index % index2 === 0) {
-            divisor += 1;
+// for (let index = 2; index <= 50; index +=1){
+//     let divisor = 0;
+//     for(let index2 = 2; index2 < index; index2 +=1){
+//         if (index % index2 === 0) {
+//             divisor += 1;
+//         }
+//     }
+//     if (divisor % index == 0){
+//     numerosPrimos.push(index)
+//     }
+// } console.log(numerosPrimos[numerosPrimos.length-1]);
+
+
+// Exercicio Bonus 1:
+
+// let n = 5;
+// let asteristico = ''
+// for (let linha = 1; linha <= n; linha +=1){
+//     for(let coluna = 1; coluna <= n; coluna +=1){
+//                 asteristico +=  '*';
+//     }
+//     console.log(asteristico)
+//     asteristico = '';
+//  }
+
+
+// Exercício bonus 22:
+
+// n = 5
+// let asteristico = '';
+
+// for(let linha = 1; linha <= n; linha += 1) {
+//     for( let coluna =1; coluna <= n; coluna +=1){
+//         if(linha === coluna) {
+//             asteristico += '*'
+//         }
+//     }
+//     console.log(asteristico)
+// }
+
+// Exercicio bonus 3:
+
+// let n = 5;
+// let espacos = '';
+// let asteristico = '';
+
+
+// for (let linha = 1; linha <= n; linha += 1) {
+//     for (let space = n - linha; space >= 1; space -= 1) {
+//         espacos += ' ';
+//     }
+//     for (let coluna = 1; coluna <= n; coluna += 1) {
+//         if(linha === coluna) {
+//             asteristico += '*';
+//         }
+//     }
+//     console.log(espacos, asteristico)
+//     espacos = '';
+// }
+
+// exercicio bonus 4;
+
+let n = 5;
+let direita = '';
+let esquerda = '';
+let asteristico = '';
+
+for (let linha = 1; linha <= n - 2; linha += 1) {
+    for (let ie = 1 ; ie <=n-2; ie += 1) {
+        if (linha <= ie)
+        esquerda += ' ';
+    }
+    for (let coluna = 1; coluna <= n-2; coluna += 1) {
+        if (linha === coluna) {
+            asteristico += '*';
         }
     }
-    if (divisor % index == 0){
-    numerosPrimos.push(index)
-    }
-} console.log(numerosPrimos[numerosPrimos.length-1]);
-    
+    console.log(esquerda, asteristico, direita)
+    asteristico += '*'
+    esquerda = '';
+}
