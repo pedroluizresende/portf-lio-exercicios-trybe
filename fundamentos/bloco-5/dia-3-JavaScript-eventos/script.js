7065eddab1b25fcc87fc9b15b6054be59c7378a5
Pedro Resende <pedroluizrnunez@gmail.com>
2022-08-29 19:13:55 -0300

    exercicio 3

--- fundamentos/bloco-5/dia-3-JavaScript-eventos/script.js
+++ fundamentos/bloco-5/dia-3-JavaScript-eventos/script.js
@@ -1,33 +1,33 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   };
-  
-  createDaysOfTheWeek();
+};
+
+createDaysOfTheWeek();
 
 
 function creaTeDaysofMounth() {
 
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let ol = document.getElementById('days');
-  for (let index =0; index < decemberDaysList.length; index += 1) {
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
     let day = decemberDaysList[index];
     const dayMounthItem = document.createElement('li');
     dayMounthItem.classList.add('day')
     dayMounthItem.innerHTML = day;
     ol.appendChild(dayMounthItem);
-    if(day === 24 || day === 25 || day === 31) {
+    if (day === 24 || day === 25 || day === 31) {
       dayMounthItem.classList.add('holiday')
     }
-    if(day === 4 || day === 11 ||day === 18 ||day === 25){
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
       dayMounthItem.classList.add('friday')
     }
   }
@@ -37,16 +37,28 @@ function creaTeDaysofMounth() {
 creaTeDaysofMounth();
 
 let buttonsContainer = document.querySelector('.buttons-container')
-function createButton(parenter, text){
-    let button = document.createElement('button');
-    button.innerText = text;
-    buttonsContainer.appendChild(button)
- console.log(button)   
- }
+let button = document.createElement('button');
+function createButton(parenter, text) {
+
+  button.innerText = text;
+  buttonsContainer.appendChild(button)
+}
+createButton(buttonsContainer, 'Feriados')
+
+button.addEventListener('click', function (event) {
+  let hollidayDays = document.querySelectorAll('.holiday')
+  for (let index = 0; index < hollidayDays.length; index += 1) {
+    if (hollidayDays[index].style.backgroundColor === 'blue') {
+      hollidayDays[index].style.backgroundColor = 'unset';
+    } else {
+      hollidayDays[index].style.backgroundColor = 'blue'
+    }
+  }
+}
+)
 
 
 
- createButton(buttonsContainer, 'Feriados')
   // function mounthDays() {
   //   for(let index in decemberDaysList) {
   //     let elementList = ol.createElement('li');
