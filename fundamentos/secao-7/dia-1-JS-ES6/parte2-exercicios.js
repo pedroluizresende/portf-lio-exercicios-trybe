9b5cf60a0ebf00f1fb82eb5c68177752403a9c6b
Pedro Resende <pedroluizrnunez@gmail.com>
2022-09-16 00:23:06 -0300

    parte 2 exercicios 4 completo

--- fundamentos/secao-7/dia-1-JS-ES6/parte2-exercicios.js
+++ fundamentos/secao-7/dia-1-JS-ES6/parte2-exercicios.js
@@ -16,21 +16,51 @@
 
 // Exercicio 2 
 
-const longestWord = (frase) => {
-  let longestWord = '';
-  let fraseArray = frase.split(' ');
-  for (let index =0; index < fraseArray.length; index +=1) {
-    longestWord = fraseArray[index].length > longestWord.length? 
-      longestWord = fraseArray[index]:
-      longestWord = longestWord
+// const longestWord = (frase) => {
+//   let longestWord = '';
+//   let fraseArray = frase.split(' ');
+//   for (let index =0; index < fraseArray.length; index +=1) {
+//     longestWord = fraseArray[index].length > longestWord.length? 
+//       longestWord = fraseArray[index]:
+//       longestWord = longestWord
   
+//   }
+//   return longestWord
+// }
+
+
+
+// console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu'));
+
+// exercicio três nos arquivos 'exercicio-3-parte2.html' e 'exercicio-3-parte2.js'
+
+// Exercício 4:
+
+const substituaX = (nome) => {
+  const frase = 'Tryber x aqui!';
+  let newFrase = '';
+  for (let index = 0; index < frase.length; index +=1) {
+    if(frase[index] === 'x') {
+      newFrase = `Tryber ${nome} aqui!`
+    }
   }
-  return longestWord
+  return newFrase;
+}
+const newFrase = substituaX('Pedro')
+const minhasSkills = (param) => {
+  const skills = ['JavaScript', 'HTML', 'CSS'];
+  let  frase = `${param}
+Minhas trẽs habilidades principais são;
+  `
+  for (let index = 0; index < skills.length; index +=1 ) {
+  frase += `- ${skills[index]}
+  `;
+  }
+  console.log(frase)
 }
 
+minhasSkills(newFrase);
 
-
-console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu'));
 
 
 
