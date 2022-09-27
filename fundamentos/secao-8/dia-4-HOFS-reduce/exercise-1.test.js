3e0da97ce6185bd754f72462a008a955082cafc5
Pedro Resende <pedroluizrnunez@gmail.com>
2022-09-27 19:05:38 -0300

    exercicio 2 completo

--- fundamentos/secao-8/dia-4-HOFS-reduce/exercise-1.test.js
+++ fundamentos/secao-8/dia-4-HOFS-reduce/exercise-1.test.js
@@ -4,4 +4,11 @@ describe('Testa a função flatten', () => {
   it('Testa se é uma função', () => {
     expect(typeof flatten).toBe('function')
   })
+  it ('Testa se retorna um array', () {
+    expect([
+      ['1', '2', '3'],
+      [true],
+      [4, 5, 6],
+    ]).toEqual(['1', '2', '3', true, 4, 5 , 6])
+  })
 })
