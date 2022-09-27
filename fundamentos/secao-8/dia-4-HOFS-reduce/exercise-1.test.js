ce63e5f76a144f20884de6dc8d62fe1b07b83516
Pedro Resende <pedroluizrnunez@gmail.com>
2022-09-27 18:20:58 -0300

    exercicio 1 completo

---
+++ fundamentos/secao-8/dia-4-HOFS-reduce/exercise-1.test.js
@@ -0,0 +1,7 @@
+const flatten = require('./exercise-1.js');
+
+describe('Testa a função flatten', () => {
+  it('Testa se é uma função', () => {
+    expect(typeof flatten).toBe('function')
+  })
+})
