d639f3798a1491debf219e959a382843f22a210e
Pedro Resende <pedroluizrnunez@gmail.com>
2022-09-28 19:04:04 -0300

    Exerrcicios do 1 ao 5 completos

---
+++ fundamentos/secao-8/dia-5-HOFS-spreadOperator-parametroRest-destructuring-e-mais/exercise2.js
@@ -0,0 +1,1 @@
+const sum = (...numbers) =>numbers.reduce((acc, number) => acc + number)
---
+++ fundamentos/secao-8/dia-5-HOFS-spreadOperator-parametroRest-destructuring-e-mais/exercise5.js
@@ -0,0 +1,6 @@
+const myList = [5, 2, 3, 4];
+// escreva swap abaixo
+
+const swap = ([value1, value2, value3]) => [value3, value2, value1];
+
+console.log(swap(myList))
